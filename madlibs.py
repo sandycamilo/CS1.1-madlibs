3d2aed79e2695412cde7e081a62d730483cbc800
sandycamilo <scam499@lasierra.edu>
2019-09-02 14:20:49 -0700

    1

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+# def madlib() :
+print('Hi! So happy you are here!')
+
+
+name = input("Enter a name")
+verb = input('Enter an -ing verb')
+adjective = input('Enter an adjective')
+noun = input('Enter a noun')
+place = input('Enter a place')
+color = input('Enter a color')
+feeling = input('Enter a feeling')
+number = input('Enter a number')
+year = input('Enter a year')
+song = input('Enter a song')
+
+print("I was sitting on a" + noun)
