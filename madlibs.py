3c640edd20a5670043dd6f534986273bc5c0985d
sandycamilo <scam499@lasierra.edu>
2019-09-03 16:52:04 -0700

    4

--- madlibs.py
+++ madlibs.py
@@ -1,3 +1,7 @@
+import os as os
+
+bashCmd = "clear"
+os.system(bashCmd)
 
 print('Hi! So happy you are here!')
 
@@ -12,19 +16,11 @@ feeling = input('Enter a feeling')
 number = input('Enter a number')
 year = input('Enter a year')
 song = input('Enter a song')
-noun2 = input('Enter a noun2')
+noun2 = input('Enter a noun (plural)')
 noun3 = input('Enter a noun3')
 
-print("I was sitting on a" + noun + " right outside my house.")
-
-print("Suddenly " + name + " pulled up on my driveway on a" + color + noun2)
-
-print("They looked at me with arms wide open and said Let's go " + verb)
-
-print("So I ran inside and grabbed my" + number + adjective + noun3)
-
-print("It was " + year + " when we finally arrived to" + place)
-
-print("We set up camp while listening to" + song)
-
-print("I was feeling" + feeling)
+print("I was sitting on a" + noun + " right outside of my house, when suddenly" + name + " drives up my driveway with"
++ color + noun2 + "." + name + " gets out of the car, looks at me in the eyes and with both arms in the air screams 'Let's go"
++ verb + "!'"".So I run inside to grab my" + adjective + noun3 + ",run back outside, hop on the passanger's seat and waved goodbye."
+"We arrived to" + place + " and proceeeded to set up camp while listening to" + song + ". I was feeling" + feeling + color +
+". It was the year of" + year)
